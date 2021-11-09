884d950f3c90353001d4a84671c1108618b5b450
joeylee1997 <joeylee97@163.com>
2021-11-09 14:31:01 +0800

    finename

--- installer/installer.groovy
+++ installer/installer.groovy
@@ -296,13 +296,14 @@ ${gitLogReport}
                             sh "git push origin HEAD:master"
                         }
                         print "更新OSS下载链接"
-                        def osslinks = sh(script: "cat \'Mirrors-for-download-(下载镜像).md\'", returnStdout: true).trim()
+                        def fineName = "\'Mirrors-for-download-(下载镜像).md\'""
+                        def osslinks = sh(script: "cat $fineName", returnStdout: true).trim()
                         print "oss links ${osslinks}"
                         if (!osslinks.contains("${params.VERSION}")) {
                             print "更新 ${params.VERSION} 到下载镜像"
-                            writeFile file: "\'Mirrors-for-download-(下载镜像).md\'", text: (MIRROS_DOWNLOAD_TEMPLATE + osslinks)
+                            writeFile file: "$fineName", text: (MIRROS_DOWNLOAD_TEMPLATE + osslinks)
                         }
-                        sh "git add \'Mirrors-for-download-(下载镜像).md\'"
+                        sh "git add $fineName"
                         sh "git commit -m \" update Mirrors for download\""
                         sh "git push origin HEAD:master"
                     }
