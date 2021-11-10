351b0c2a2fe897d18ada55983ee9516ee9a34957
joeylee1997 <joeylee97@163.com>
2021-11-10 17:22:39 +0800

    weekly update

---
+++ utils/publisher.groovy
@@ -0,0 +1,26 @@
+
+pipeline {
+    agent any
+    stages {
+        stage('text'){
+            steps {
+                echo '测试 TEXT 消息...'
+            }
+            post {
+                success {
+                    dingtalk (
+                            robot: '2',
+                            type: '',
+                            title: 'Dragonwell Weekly Report',
+                            text: [
+                                    '# Github 状态',
+                                    '[Dragonwell8下载报告](https://qii404.me/github-release-statistics/?repo=https://github.com/alibaba/dragonwell8.git)',
+                                    '[Dragonwell11下载报告](https://qii404.me/github-release-statistics/?repo=https://github.com/alibaba/dragonwell11.git)',
+                                    '[Dragonwell17下载报告](https://qii404.me/github-release-statistics/?repo=https://github.com/alibaba/dragonwell17.git)'
+                            ]
+                    )
+                }
+            }
+        }
+    }
+}
