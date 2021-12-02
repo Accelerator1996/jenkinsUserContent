e086ac3f97138548ea95d38cb2e6f92f961356d2
joeylee1997 <joeylee97@163.com>
2021-12-02 16:14:57 +0800

    china

--- installer/installer.groovy
+++ installer/installer.groovy
@@ -60,6 +60,12 @@ if (params.RELEASE == "8") {
     BUILDER = "http://ci.dragonwell-jdk.io/userContent/utils/build17.sh"
 }
 
+
+// for Chinese and English version
+def updateReleaseNotes() {
+
+}
+
 pipeline {
     options {
         durabilityHint('PERFORMANCE_OPTIMIZED')
@@ -283,6 +289,24 @@ ${gitLogReport}
                             sh "git commit -m \" update Alibaba-Dragonwell-${params.RELEASE}-Release-Notes.md \""
                             sh "git push origin HEAD:master"
                         }
+                        if (fileExists "阿里巴巴Dragonwell${params.RELEASE}发布说明.md") {
+                            print "更新 ${params.VERSION} 到 发布说明中文版"
+                            releasenots = sh(script: "cat 阿里巴巴Dragonwell${params.RELEASE}发布说明.md", returnStdout: true).trim()
+                            def gitLogReport = sh(script: "python3 driller.py --repo /repo/dragonwell${params.RELEASE} ${fromTag} --totag master", returnStdout: true)
+                            def newReleasenotes = """
+# ${params.VERSION}
+ ```
+${fullVersionOutput}
+ ```
+${gitLogReport}
+                            """ + releasenots
+                            writeFile file: "阿里巴巴Dragonwell${params.RELEASE}发布说明.md", text: newReleasenotes
+                            sh "git add A阿里巴巴Dragonwell${params.RELEASE}发布说明.md"
+                            sh "git commit -m \" update 阿里巴巴Dragonwell${params.RELEASE}发布说明.md \""
+                            sh "git push origin HEAD:master"
+
+                        }
+
                         print "更新docker镜像"
                         def dockerimages = sh(script: "cat Use-Dragonwell-${params.RELEASE}-docker-images.md", returnStdout: true).trim()
 
@@ -312,7 +336,7 @@ ${gitLogReport}
                         print "oss links ${osslinks}"
                         if (!osslinks.contains("${params.VERSION}")) {
                             print "更新 ${params.VERSION} 到下载镜像"
-                            writeFile file: 'Mirrors-for-download-(下载镜像).md', text: (osslinks + MIRROS_DOWNLOAD_TEMPLATE)
+                            writeFile file: 'Mirrors-for-download-(下载镜像).md', text: (MIRROS_DOWNLOAD_TEMPLATE + osslinks)
                             sh "git add 'Mirrors-for-download-(下载镜像).md'"
                             sh "git commit -m \" update Mirrors for download\""
                             sh "git push origin HEAD:master"
